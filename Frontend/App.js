2136a4f83ca2121ca294243264f23aecdba33344
Nissan <60010275@kmitl.ac.th>
2020-09-18 11:09:51 +0700

    front end fix bug

--- Frontend/App.js
+++ Frontend/App.js
@@ -3,66 +3,80 @@ import React from "react";
 import { StyleSheet, View, SafeAreaView, Text, Alert } from "react-native";
 import { Input, Button } from "react-native-elements";
 import { Header } from "react-native-elements";
+import { render } from "react-dom";
 
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      date:[]
+    }
+  }
+  componentDidMount(){
+    this.apiCall();
+  }
+  async apiCall(){
+    let resp = await fetch('http://localhost:8000/api/hello')
+    console.log(resp.message)
 
-export default function App() {
-  const handlePress = () => false;
-  console.log("app executed");
-  return (
-    <View>
-      <Header
-        centerComponent={{
-          text: "Task",
-          style: { color: "#fff", fontSize: 20, fontWeight: "bold" },
-        }}
-        rightComponent={{
-          text: "CE COIN",
-          style: { color: "#fff", fontSize: 15, fontWeight: "bold" },
-        }}
-        containerStyle={{
-          backgroundColor: "#333333",
-          justifyContent: "space-around",
-        }}
-      />
-      <View style={{ alignItems: "center", justifyContent: "center" }}>
-        <View style={{ marginTop: 20 }}>
+  }
+    render(){
+    const handlePress = () => console.log("on Pressed")
+    return (
+      <View>
+        <Header
+          centerComponent={{
+            text: "Task",
+            style: { color: "#fff", fontSize: 20, fontWeight: "bold" },
+          }}
+          rightComponent={{
+            text: "CE COIN",
+            style: { color: "#fff", fontSize: 15, fontWeight: "bold" },
+          }}
+          containerStyle={{
+            backgroundColor: "#333333",
+            justifyContent: "space-around",
+          }}
+        />
+        <View style={{ alignItems: "center", justifyContent: "center" }}>
+          <View style={{ marginTop: 20 }}>
+            <Button
+              onPress={handlePress}
+              titleStyle={{
+                color: "white",
+                fontSize: 20,
+              }}
+              buttonStyle={{
+                backgroundColor: "#FF6F07",
+                borderRadius: 6,
+                height: 52,
+                width: 380,
+              }}
+              title="Class Check-in"
+            />
+          </View>
+          <View style={{ marginTop: 20 }}>
           <Button
-            onPress={handlePress}
-            titleStyle={{
-              color: "white",
-              fontSize: 20,
-           
-            }}
-            buttonStyle={{
-              backgroundColor: "#FF6F07",
-              borderRadius: 6,
-              height: 52,
-              width: 380,
-            }}
-            title="Class Check-in"
-          />
-        </View>
-        <View style={{ marginTop: 20 }}>
-        <Button
-            onPress={handlePress}
-            titleStyle={{
-              color: "white",
-              fontSize: 20,
-            
-            }}
-            buttonStyle={{
-              backgroundColor: "#FF4D15",
-              borderRadius: 6,
-              height: 52,
-              width: 380,
-            }}
-            title="Get Coin By Code"
-          />
+              onPress={this.apiCall}
+              titleStyle={{
+                color: "white",
+                fontSize: 20,
+              }}
+              buttonStyle={{
+                backgroundColor: "#FF4D15",
+                borderRadius: 6,
+                height: 52,
+                width: 380,
+              }}
+              title="Get Coin By Code"
+            />
+          </View>
         </View>
       </View>
-    </View>
-  );
-}
+    )
+        }
+      }
+export default App ;
 
 const styles = StyleSheet.create({
   container: {
